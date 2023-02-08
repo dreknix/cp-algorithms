00f8ba0605e50e9b634f7429791b2600cde4d67b
dreknix <dreknix@proton.me>
2023-02-08 15:48:49 +0100

    Adding Murmur hash

--- map/main.c
+++ map/main.c
@@ -57,38 +57,82 @@ uint32_t map_capacity(map_t map) { return map->capacity; }
 uint32_t map_size(map_t map) { return map->size; }
 
 // https://github.com/aappleby/smhasher/blob/master/src/MurmurHash3.cpp
+inline uint32_t rotl32(uint32_t x, int8_t r) {
+    return (x << r) | (x >> (32 - r));
+}
+
+inline uint32_t getblock32(const uint32_t * p, int i) {
+    return p[i];
+}
+
+inline uint32_t fmix32(uint32_t h) {
+    h ^= h >> 16;
+    h *= 0x85ebca6b;
+    h ^= h >> 13;
+    h *= 0xc2b2ae35;
+    h ^= h >> 16;
+
+    return h;
+}
+
+void MurmurHash3_x86_32(const void *key, int len, uint32_t seed, void *out) {
+  const uint8_t *data = (const uint8_t*)key;
+  const int nblocks   = len / 4;
+
+  uint32_t h1 = seed;
+
+  const uint32_t c1 = 0xcc9e2d51;
+  const uint32_t c2 = 0x1b873593;
+
+  //----------
+  // body
+
+  const uint32_t *blocks = (const uint32_t *)(data + nblocks*4);
+
+  for (int i = -nblocks; i; i++) {
+      uint32_t k1 = getblock32(blocks,i);
+
+      k1 *= c1;
+      k1  = rotl32(k1,15);
+      k1 *= c2;
+
+      h1 ^= k1;
+      h1  = rotl32(h1,13);
+      h1  = h1 * 5 + 0xe6546b64;
+  }
+
+  //----------
+  // tail
+
+  const uint8_t *tail = (const uint8_t*)(data + nblocks*4);
+
+  uint32_t k1 = 0;
+
+  switch(len & 3) {
+    case 3:
+      k1 ^= tail[2] << 16;
+    case 2:
+      k1 ^= tail[1] << 8;
+    case 1:
+      k1 ^= tail[0];
+      k1 *= c1;
+      k1  = rotl32(k1,15);
+      k1 *= c2;
+      h1 ^= k1;
+  };
+
+  //----------
+  // finalization
+
+  h1 ^= len;
+  h1  = fmix32(h1);
+
+  *(uint32_t*)out = h1;
+}
+
 static uint32_t generate_hash(uint64_t key) {
-    const uint8_t *data = (uint8_t *)&key;
-
-    // largest prime in uint32_t: 4.294.967.291 < 4.294.967.295
-    uint32_t hash = 4294967291; // seed
-
-    const uint32_t c1 = 0xcc9e2d51;
-    const uint32_t c2 = 0x1b873593;
-
-    uint32_t k1;
-    for (int i = 0; i < 8; i++) {
-        k1  = data[i];
-        k1 *= c1;
-        k1  = (k1 << 15) | (k1 >> (32 - 15));
-        k1 *= c2;
-
-        hash ^= k1;
-        hash  = (hash << 13) | (hash >> (32 - 13));
-        hash  = hash * 5 + 0xe6546b64;
-    }
-
-    k1   ^= data[0];
-    k1   *= c1;
-    k1    = (k1 << 15) | (k1 >> (32 - 15));
-    hash ^= k1;
-
-    hash ^= hash >> 16;
-    hash *= 0x85ebca6b;
-    hash ^= hash >> 13;
-    hash *= 0xc2b2ae35;
-    hash ^= hash >> 16;
-
+    uint32_t hash;
+    MurmurHash3_x86_32 (&key, 8, 0xdeadbeef, &hash);
     return hash;
 }
 
@@ -116,7 +160,7 @@ static entry_t *map_find_entry(map_t map, uint64_t key, bool empty) {
         }
         index = (index + skip) % map->capacity;
         entry = &map->entries[index];
-        skip+=3;
+        // TODO skip+=3;
     }
 
     if (empty && entry->key != key && index != hash % map->capacity) {
